649070648d9ae0d7cd07827f325e8ffbf34d605d
Ralf Biedert <rb@xr.io>
2013-02-18 22:02:57 +0100

    Added "primitive float" which will help us with the Ani library.

---
+++ runtime/src/jycessing/PrimitiveFloat.java
@@ -0,0 +1,13 @@
+package jycessing;
+
+public class PrimitiveFloat {
+	public PrimitiveFloat(float value) {
+		this.value = value;
+	}
+
+	public float value = 0.0f;
+
+	public String toString() {
+		return "" + value;
+	}
+}
