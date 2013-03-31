da70347f7dd6b21668933e753255ab3edff28e8d
Ralf Biedert <rb@xr.io>
2013-03-31 14:05:32 +0200

    Also added development launcher scripts / apps.

---
+++ libraries/README.txt
@@ -0,0 +1,4 @@
+
+
+Extract any official Processing 2.0 plug-in into the "processing" folder.
+
---
+++ workspace/example.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+def setup():
+	size(640, 480, P2D)
+
+
+def draw():
+	background(0)
+	ellipse(mouseX, mouseY, 10, 10)
