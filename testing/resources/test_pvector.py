7f9af4017d13c6d272f489dd07f888f90df0b949
James Gilles <jameshgilles@gmail.com>
2014-04-19 23:31:28 -0400

    Added PVector tests

--- testing/resources/test_pvector.py
+++ testing/resources/test_pvector.py
@@ -12,6 +12,7 @@ assert a == c
 a.add(b)
 assert a == PVector(31.0, 41.0, 57.0)
 
+
 try:
     print a * b
     raise AssertionError("That shouldn't have happened.")
@@ -31,6 +32,31 @@ assert PVector.cross(a, b) == PVector(-260.0, 280.0, -60.0)
 assert a.cross(b) == PVector(-260.0, 280.0, -60.0)
 assert PVector.dot(a, b) == 24110.0
 
+d = a.get()
+d += b
+assert d == a + b
+d = a.get()
+d -= c
+assert d == a - c
+d = a.get()
+d *= 5.0
+assert d == a * 5.0
+d = a.get()
+d /= 5.0
+assert d == a / 5.0
+
+d = a.get()
+d += a + a
+assert d == a + a + a
+
+assert a * 57.0 == 57.0 * a
+
+assert (a / 5.0) == (1.0 / 5.0) * a
+
+m, n = b, c
+a += b * 5 - c / 2 + PVector(0, 1, 2)
+assert (m, n) == (b, c)
+
 import copy
 x = [a, b]
 y = copy.deepcopy(x)
@@ -38,6 +64,8 @@ y = copy.deepcopy(x)
 assert x == y
 x[0].sub(PVector(100, 100, 100))
 assert x != y
+
+
 
 print 'OK'
 
