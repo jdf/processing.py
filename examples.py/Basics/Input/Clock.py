85a1a852ffc06c45cf2efc85d061bdbc9677049e
Miles Peyton <milespeyton@gmail.com>
2014-05-19 17:51:07 -0400

    Update Clock.py

--- examples.py/Basics/Input/Clock.py
+++ examples.py/Basics/Input/Clock.py
@@ -1,10 +1,10 @@
 """
-  Clock. 
+Clock. 
   
-  The current time can be read with the second(), minute(), 
-  and hour() functions. In this example, sin() and cos() values
-  are used to set the position of the hands.
- """
+The current time can be read with the second(), minute(), 
+and hour() functions. In this example, sin() and cos() values
+are used to set the position of the hands.
+"""
  
 def setup():
     size(640, 360)
