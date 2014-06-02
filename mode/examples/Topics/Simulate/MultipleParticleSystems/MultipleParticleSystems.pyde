8d945377f4b3a08a525f138bcf8e8284739fdaab
Miles Peyton <milespeyton@gmail.com>
2014-06-02 14:30:59 -0400

    Started crazy_particle.

--- mode/examples/Topics/Simulate/MultipleParticleSystems/MultipleParticleSystems.pyde
+++ mode/examples/Topics/Simulate/MultipleParticleSystems/MultipleParticleSystems.pyde
@@ -9,20 +9,26 @@ Each burst is one instance of a particle system
 with Particles and CrazyParticles (a subclass of(object): Particle)
 Note use of Inheritance and Polymorphism here.
 """
-ArrayList<ParticleSystem> systems
-def setup(): 
+systems = None
+
+
+def setup():
     size(640, 360)
-    systems = ArrayList<ParticleSystem>()
-def draw(): 
+    systems = []
+
+
+def draw():
     background(0)
-    for (ParticleSystem ps: systems) 
+    for (ps in systems)
         ps.run()
         ps.addParticle()
-    
+
     if systems.isEmpty():
         fill(255)
         textAlign(CENTER)
-        text("click mouse to add particle systems", width/2, height/2)
-    
-def mousePressed(): 
+        text("click mouse to add particle systems", width / 2, height / 2)
+
+
+def mousePressed():
     systems.add(ParticleSystem(1, PVector(mouseX, mouseY)))
+
--- mode/examples/Topics/Simulate/MultipleParticleSystems/crazy_particle.py
+++ mode/examples/Topics/Simulate/MultipleParticleSystems/crazy_particle.py
@@ -1,32 +1,33 @@
-# A subclass of(object): Particle
-class CrazyParticle(object): extends Particle 
-    # Just adding one variable to a CrazyParticle
-    # It inherits all other fields from "Particle", and we don't have to retype them!
-    theta
-    # The CrazyParticle constructor can call the parent class (super class) constructor
-    CrazyParticle(PVector l) 
-        # "super" means do everything from the constructor in Particle
+# A subclass of Particle
+class CrazyParticle(Particle):
+    # Just adding one variable to a CrazyParticle.
+    # It inherits all other fields from "Particle", and we don't have to
+    # retype them!
+    # The CrazyParticle constructor can call the parent class (super class)
+    # constructor.
+    CrazyParticle(self, l)
+        # "super" means do everything from the constructor in Particle.
         super(l)
-        # One more line of code to deal with the variable, theta
-        theta = 0.0
-    
+        # One more line of code to deal with the variable, theta.
+        self.theta = 0.0
+
     # Notice we don't have the method run() hereit is inherited from Particle
-    # This update() method overrides the parent class update(object):() method
-    def update(): 
+    # This update() method overrides the parent class update()
+    def update(self):
         super.update()
-        # Increment rotation based on horizontal velocity
+        # Increment rotation based on horizontal velocity.
         theta_vel = (velocity.x * velocity.mag()) / 10.0f
         theta += theta_vel
-    
-    # This display() method overrides the parent class display(object):() method
-    def display(): 
+
+    # This display() method overrides the parent class display() method.
+    def display(self):
         # Render the ellipse just like in a regular particle
         super.display()
         # Then add a rotating line
         pushMatrix()
-        translate(location.x,location.y)
+        translate(location.x, location.y)
         rotate(theta)
-        stroke(255,lifespan)
-        line(0,0,25,0)
+        stroke(255, lifespan)
+        line(0, 0, 25, 0)
         popMatrix()
-    
+
