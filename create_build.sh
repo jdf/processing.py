7929a0ba581dbe37138c2b055910411daf8cf6ae
Jonathan Feinberg <jdf@pobox.com>
2014-06-14 13:49:11 -0400

    Build 0355.

--- create_build.sh
+++ create_build.sh
@@ -3,8 +3,8 @@
 echo -n 'Bumping version...'
 version=$(ant bumpversion | perl -lne '/Bumped to version (\d+)/ && print $1')
 echo $version
-git commit -am "Build $1." && \
-	git tag $1 && \
+git commit -am "Build $version." && \
+	git tag $version && \
 	git push && \
 	git push --tags && \
 	ant mode.upload
