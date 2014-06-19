7a1f976a62584b7e98359a5c1b18a9f298838ba5
James Gilles <jameshgilles@gmail.com>
2014-06-19 17:59:01 -0400

    Switched to single export() function per platform

--- runtime/src/jycessing/mode/export/Exporter.java
+++ runtime/src/jycessing/mode/export/Exporter.java
@@ -33,77 +33,15 @@ public class Exporter {
   }
   
   public void export() {
+    // Do export-common things - work out libraries, etc.
+    List<Library> libraries = new ArrayList<Library>();
+    
+    // Now, do this for each platform:
     try {
-      exportWith(new LinuxExport(64));
+      new LinuxExport(64, sketch, libraries).export();
     } catch (IOException e) {
       e.printStackTrace();
       editor.statusError("Export failed!");
     }
-  }
-  
-  private void exportWith(PlatformExport platform) throws IOException {
-    
-    log("Exporting to "+platform.name);
-    
-    // Work out user preferences and other possibilities we care about
-    // I'm putting these here to tell anyone reading what this method depends on - does that make sense?
-    final boolean embedJava = (platform.id == PApplet.platform) && Preferences.getBoolean("export.application.embed_java");
-    final boolean hasData = sketch.hasDataFolder();
-    final boolean hasCode = sketch.hasCodeFolder();
-    
-    // Work out the folders we'll be (maybe) using
-    final File destFolder = new File(sketch.getFolder(), "application."+platform.name);
-    final File libFolder = new File(destFolder, "lib");
-    
-    // Things we need to keep track of
-    List<Library> libraries = new ArrayList<Library>(); // Obtained by scraping the python AST, eventually...
-    List<String> jarFiles = new ArrayList<String>();    // Things we'll need to add to the export's classpath
-    
-    // Delete previous export (if the user wants to, and it exists) and make a new one
-    platform.prepareExportFolder(sketch);
-    
-    // Handle embedding java
-    if (embedJava) {
-      log("Embedding java in export.");
-      platform.embedJava(sketch);
-    }
-    
-    // Handle data folder
-    if (hasData) {
-      log("Copying data folder to export.");
-      platform.copyData(sketch);
-    }
-    
-    // Handle code folder (for .jar files that aren't formatted as Processing libraries... and more?)
-    if (hasCode) {
-      log("Copying code folder to export.");
-      platform.copyCode(sketch);
-    }
-    
-    // Handle source folder
-    {
-      log("Copying source to export.");
-      platform.copySource(sketch);
-    }
-    
-    // Handle imported libraries
-    // For now, all we have is the core library
-    {
-      Library core = new Library(Base.getContentFile("core"));
-      libraries.add(core);
-      
-      platform.copyLibraries(sketch, libraries);
-    }
-    
-    // Add Python Mode stuff (pymode jar, guava, jython jar, jython license)
-    // [If we restructured the mode code as a P5 library this could be folded into the above]
-    for (File exportFile : editor.getRequiredFiles()){
-      if (exportFile.getName().toLowerCase().endsWith(".jar")) {
-        jarFiles.add(exportFile.getName());
-      }
-      Base.copyFile(exportFile, new File(libFolder, exportFile.getName()));
-    }
-    
-    platform.createExecutable(sketch);
   }
 }
