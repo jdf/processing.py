46e1be2ba1672beb6a28268bf5d5e82d5dae6379
James Gilles <jameshgilles@gmail.com>
2014-06-19 17:35:53 -0400

    Bring some code back

--- runtime/src/jycessing/mode/export/LinuxExport.java
+++ runtime/src/jycessing/mode/export/LinuxExport.java
@@ -1,12 +1,16 @@
 package jycessing.mode.export;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 import java.io.IOException;
 
 import processing.app.Base;
 import processing.app.Library;
+import processing.app.Preferences;
 import processing.app.Sketch;
+import processing.app.SketchCode;
+import processing.core.PApplet;
 import processing.core.PConstants;
 
 public class LinuxExport extends PlatformExport {
@@ -78,5 +82,109 @@ public class LinuxExport extends PlatformExport {
     // TODO Auto-generated method stub
 
   }
-
+  
+  public void export() {
+ // Work out user preferences and other possibilities we care about
+    /*
+    // I'm putting these here to tell anyone reading what this method depends on - does that make sense?
+    final boolean embedJava = (platform == PApplet.platform) && Preferences.getBoolean("export.application.embed_java");
+    final boolean hasData = sketch.hasDataFolder();
+    final boolean hasCode = sketch.hasCodeFolder();
+    
+    // Work out the folders we'll be (maybe) using
+    final File destFolder = new File(sketch.getFolder(), "application."+PConstants.platformNames[platform]+bits);
+    final File libFolder = new File(destFolder, "lib");
+    final File codeFolder = new File(destFolder, "code");
+    final File sourceFolder = new File(destFolder, "source");
+    final File dataFolder = new File(destFolder, "data");
+    final File javaFolder = new File(destFolder, "java");
+    
+    // Things we need to keep track of
+    List<Library> libraries = new ArrayList<Library>(); // Obtained by scraping the python AST, eventually...
+    List<String> jarFiles = new ArrayList<String>();    // Things we'll need to add to the export's classpath
+    
+    // Delete previous export (if the user wants to, and it exists) and make a new one
+    pyMode.prepareExportFolder(destFolder);
+    
+    // Handle embedding java
+    if (embedJava) {
+      log("Embedding java in export.");
+      javaFolder.mkdirs();
+      if (platform == PConstants.MACOSX) {
+        
+      } else if (platform == PConstants.WINDOWS) {
+        
+      } else if (platform == PConstants.LINUX) {
+        Base.copyDir(Base.getJavaHome(), javaFolder);
+      } else if (platform == PConstants.OTHER) {
+        
+      }
+    }
+    
+    // Handle data folder
+    if (hasData) {
+      log("Copying data folder to export.");
+      dataFolder.mkdirs();
+      if (platform == PConstants.MACOSX) {
+        
+      } else if (platform == PConstants.WINDOWS) {
+        
+      } else if (platform == PConstants.LINUX) {
+        Base.copyDir(sketch.getDataFolder(), dataFolder);
+      } else if (platform == PConstants.OTHER) {
+        
+      }
+    }
+    
+    // Handle code folder
+    // ...what is the code folder for, exactly?
+    if (hasCode) {
+      log("Copying code folder to export.");
+      codeFolder.mkdirs();
+      if (platform == PConstants.MACOSX) {
+        
+      } else if (platform == PConstants.WINDOWS) {
+        
+      } else if (platform == PConstants.LINUX) {
+        
+      } else if (platform == PConstants.OTHER) {
+        
+      }
+    }
+    
+    // Handle source folder
+    {
+      log("Copying source to export.");
+      sourceFolder.mkdirs();
+      for (SketchCode code : sketch.getCode()){
+        code.copyTo(new File(sourceFolder, code.getFileName()));
+      }
+    }
+    
+    // Handle imported libraries
+    // For now, all we have is the core library and the processing.py library
+    {
+      Library core = new Library(Base.getContentFile("core"));
+      libraries.add(core);
+      for (Library library : libraries) {
+        for (File exportFile : library.getApplicationExports(platform, bits)) {
+          final String exportName = exportFile.getName();
+          if (!exportFile.exists()) {
+            statusError("The file "+exportName+" is mentioned in the export.txt from "
+                          +library+" but does not actually exist.");
+            continue;
+          }
+          if (exportFile.isDirectory()) {
+            Base.copyDir(exportFile, new File(libFolder, exportName));
+          } else if (exportName.toLowerCase().endsWith(".jar") || exportName.toLowerCase().endsWith(".zip")) {
+            jarFiles.add(exportName);
+            Base.copyFile(exportFile, new File(libFolder, exportName));
+          } else {
+            Base.copyFile(exportFile, new File(libFolder, exportName));
+          }
+        }
+      }
+    }*/
+  }
+  
 }
