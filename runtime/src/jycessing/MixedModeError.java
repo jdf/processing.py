f44c94c861228105368ede3b4e619614255c07cc
Jonathan Feinberg <jdf@pobox.com>
2014-09-02 08:01:22 -0400

    Fix typo in MixedModeError message.

--- runtime/src/jycessing/MixedModeError.java
+++ runtime/src/jycessing/MixedModeError.java
@@ -2,6 +2,6 @@ package jycessing;
 
 public class MixedModeError extends PythonSketchError {
   public MixedModeError() {
-    super("It looks like you're mising \"active\" and \"static\" modes.");
+    super("It looks like you're mixing \"active\" and \"static\" modes.");
   }
 }
