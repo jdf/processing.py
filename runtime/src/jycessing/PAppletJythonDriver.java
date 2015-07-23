471393440213235062b74c27ee6ef43b14191f9a
Luxapodular <luca.damasco@gmail.com>
2015-07-23 16:54:51 -0400

    Removing Prints...

--- runtime/src/jycessing/PAppletJythonDriver.java
+++ runtime/src/jycessing/PAppletJythonDriver.java
@@ -168,7 +168,6 @@ public class PAppletJythonDriver extends PApplet {
   private SketchPositionListener sketchPositionListener;
 
   private void processSketch(final String scriptSource) throws PythonSketchError {
-    System.out.println("Processing Sketch");
     try {
       /*
        * Run the Python
