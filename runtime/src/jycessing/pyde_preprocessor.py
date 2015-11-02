675ededa6b4fb26da759650d206913478d216c44
Jonathan Feinberg <jdf@pobox.com>
2015-11-01 23:06:12 -0500

    Cleanup pyde preprocessor code.

--- runtime/src/jycessing/pyde_preprocessor.py
+++ runtime/src/jycessing/pyde_preprocessor.py
@@ -30,58 +30,49 @@ class Program_info():
 		self.noSmooth = False
 		self.smooth = False
 
+	def insert(self, body):
+		for attr in ('size', 'fullScreen', 'noSmooth', 'smooth'):
+			if getattr(self, attr):
+				body.insert(0, getattr(self, attr))
+		
 def pyde_preprocessor(module):
-	__program_info__ = Program_info()
+	info = Program_info()
     # Walk throught the abstract syntax tree for the original sketch. 
 	for node in module.body:
 		if isinstance(node, ast.FunctionDef):
 			if (node.name == 'setup'):
 				# The user has defined a setup() function. Look through setup() for calls
 				# to size(), fullScreen(), noSmooth() and smooth(). 
-				__program_info__.found_setup = True
+				info.found_setup = True
 				for subNode in node.body:
-					if isinstance(subNode, ast.Expr):
-						if isinstance(subNode.value, ast.Call):
-							func = subNode.value.func
-							if hasattr(func, 'id') and func.id in ('size', 'fullScreen', 'noSmooth', 'smooth'):
-								node.body.remove(subNode)
-								setattr(__program_info__, func.id, subNode)
+					if not isinstance(subNode, ast.Expr):
+						continue
+					if not isinstance(subNode.value, ast.Call):
+						continue
+					func = subNode.value.func
+					if hasattr(func, 'id') and func.id in ('size', 'fullScreen', 'noSmooth', 'smooth'):
+						node.body.remove(subNode)
+						setattr(info, func.id, subNode)
 			elif (node.name == 'settings'):
 				# The user has defined a settings() function. 
-				__program_info__.found_settings = True
+				info.found_settings = True
 
-	if (__program_info__.size or __program_info__.fullScreen or __program_info__.noSmooth or __program_info__.smooth):
+	if (info.size or info.fullScreen or info.noSmooth or info.smooth):
 		# The user called one of the settings() subfunctions inside setup.
-		if (__program_info__.found_settings):
+		if (info.found_settings):
 			# If a settings function was already defined, go through the tree to find it.
 			# Place all of the special function calls inside settings function body. 
 			for node in module.body:
 				if (isinstance(node, ast.FunctionDef)):
 					if (node.name == 'settings'):
-						if (__program_info__.smooth):
-							node.body.insert(0, __program_info__.smooth)
-						if (__program_info__.noSmooth):
-							node.body.insert(0, __program_info__.noSmooth)
-						if (__program_info__.size):
-							node.body.insert(0, __program_info__.size)
-						if (__program_info__.fullScreen):
-							node.body.insert(0, __program_info__.fullScreen)
-						# Don't look through the rest of the tree. 
+						info.insert(node.body)
 						break
 		else:
 			# If a settings function is not defined, we define one and place all of 
 			# the special function calls within it. 
 			settingsArgs = ast.arguments(args = [], vararg = None, kwarg = None, defaults = [])
 			settingsFunc = ast.FunctionDef("settings",settingsArgs,[],[])
-			if (__program_info__.noSmooth):
-				settingsFunc.body.insert(0, __program_info__.noSmooth)
-			if (__program_info__.smooth):
-				settingsFunc.body.insert(0, __program_info__.smooth)
-			if (__program_info__.size):
-				settingsFunc.body.insert(0, __program_info__.size)
-			if (__program_info__.fullScreen):
-				settingsFunc.body.insert(0, __program_info__.fullScreen)
-
+			info.insert(settingsFunc.body)
 			# Place the newly defined settings() function within the module body. 
 			# It's like it's been there the whole time... 
 			module.body.insert(0, settingsFunc)
