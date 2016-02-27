a463c5ee586b55d71d0b62aa6a0274d730d481df
Prabhjot Singh <prabhjot.nith@gmail.com>
2016-02-27 18:37:53 +0530

    Update MoveEye.py
    removed Java style comments

--- examples.py/3D/Camera/MoveEye.py
+++ examples.py/3D/Camera/MoveEye.py
@@ -1,22 +1,26 @@
 """
- * Move Eye. 
- * by Simon Greenwold.
- * 
- * The camera lifts up (controlled by mouseY) while looking at the same point.
+ Move Eye. 
+ by Simon Greenwold.
+  
+ The camera lifts up (controlled by mouseY) while looking at the same point.
+ The camera has no effect while moving from left to right i.e.
+ unaffected by mouseX.
  """
-def setup(): 
+ 
+def setup():
     size(640, 360, P3D)
     fill(204)
-    
-def draw(): 
+
+
+def draw():
     lights()
     background(0)
-    
+
     # Change height of the camera with mouseY
-    camera(30.0, mouseY, 220.0, # eyeX, eyeY, eyeZ
-           0.0, 0.0, 0.0,       # centerX, centerY, centerZ
-           0.0, 1.0, 0.0)       # upX, upY, upZ
-    
+    camera(30.0, mouseY, 220.0,  # eyeX, eyeY, eyeZ
+           0.0, 0.0, 0.0,        # centerX, centerY, centerZ
+           0.0, 1.0, 0.0)        # upX, upY, upZ
+
     noStroke()
     box(90)
     stroke(255)
