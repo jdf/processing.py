3994a9823774ea26bd962a9f9f04ce3838a80ddf
Prabhjot Singh <prabhjot.nith@gmail.com>
2016-02-29 17:58:11 +0530

    Update RGBCube.py

--- examples.py/3D/Form/RGBCube.py
+++ examples.py/3D/Form/RGBCube.py
@@ -1,21 +1,21 @@
-"""
- * RGB Cube.
- * 
- * The three primary colors of the additive color model are red, green, and blue.
- * This RGB color cube displays smooth transitions between these colors. 
- """
+'''
+ RGB Cube.
+ 
+ The three primary colors of the additive color model are Red, Green and Blue.
+ This RGB color cube displays a smooth transitions between these colors. 
+'''
  
 xmag, ymag = (0, 0)
 newXmag, newYmag = (0, 0) 
- 
+
 def setup(): 
     size(640, 360, P3D) 
     noStroke() 
     colorMode(RGB, 1) 
  
+
 def draw():
     global xmag, ymag, newXmag, newYmag
-     
     background(0.5)
 
     pushMatrix() 
@@ -27,14 +27,14 @@ def draw():
     diff = xmag - newXmag
     if abs(diff) > 0.01:
         xmag -= diff / 4.0 
-    
+ 
     diff = ymag - newYmag
     if abs(diff) > 0.01:
         ymag -= diff / 4.0 
-    
+
     rotateX(-ymag) 
     rotateY(-xmag) 
-    
+
     scale(90)
     beginShape(QUADS)
     fill(0, 1, 1)
@@ -87,6 +87,5 @@ def draw():
     vertex(-1, -1, 1)
 
     endShape()
-    
     popMatrix() 
  
