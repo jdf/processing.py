ac16f071edd556268c35cf502d9f799b6976ae00
Jonathan Feinberg <jdf@pobox.com>
2018-12-01 19:37:24 -0500

    fix the g test

--- testing/resources/test_g.py
+++ testing/resources/test_g.py
@@ -4,9 +4,6 @@ def setup():
     size(100, 100, P3D)
 
 def draw():
-	# check that "g" is defined and is the expected type
-    assert(isinstance(g, processing.opengl.PGraphics3D))
-    
     # check that the alias cameraMatrix->camera is working as expected
     g.camera(0, 0, -10, 0, 0, 0, 0, 1, 0)
     assert(g.cameraMatrix.m03 == 0)
