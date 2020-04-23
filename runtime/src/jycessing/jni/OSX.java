eeb9e5acfdc268f42bc5ec5077957edd661b29af
Molly Stuart <mparkerstuart@gmail.com>
2020-04-23 07:06:56 -0500

    Bring IDE to front when a sketch finishes on OSX
    The IDE runs in a java environment whos java.library.path doesn't
    include the directory containing jniosx, so the library must be loaded
    using a path to the file. Overloaded OSX.bringToFront to allow using a
    PythonMode object to create the path and load jniosx so we can bring the
    IDE to front.
    
    Removed the static initializer that loaded jniosx and moved the
    System.loadLibrary command inside bringToFront so the two different
    methods of loading the library wouldn't step on each other.
    
    Added brinToFront(mode) to PyEditor to bring the IDE to
    front.

--- runtime/src/jycessing/jni/OSX.java
+++ runtime/src/jycessing/jni/OSX.java
@@ -1,13 +1,14 @@
 package jycessing.jni;
 
+import jycessing.mode.PythonMode;
 import processing.core.PApplet;
 import processing.core.PConstants;
 
 public class OSX {
   private static volatile boolean didLoad = false;
 
-  static {
-    if (PApplet.platform == PConstants.MACOSX) {
+  public static void bringToFront() {
+    if (!didLoad && (PApplet.platform == PConstants.MACOSX)) {
       try {
         System.loadLibrary("jniosx");
         didLoad = true;
@@ -15,12 +16,23 @@ public class OSX {
         System.err.println("Hmm. Can't load native code to bring window to front.");
       }
     }
-  }
-
-  public static void bringToFront() {
     if (didLoad) {
       activateIgnoringOtherApps();
     }
+  }
+
+  public static void bringToFront(PythonMode mode) {
+    if (!didLoad && (PApplet.platform == PConstants.MACOSX)) {
+      String path = null;
+      try {
+        path = mode.getContentFile("mode").getAbsolutePath() + "/libjniosx.dylib";
+        System.load(path);
+        didLoad = true;
+      } catch (final UnsatisfiedLinkError err) {
+        System.err.println("Hmm. Can't load native code to bring window to front using the absolute path: " + path + ".");
+      }
+    }
+    bringToFront();
   }
 
   private static native void activateIgnoringOtherApps();
